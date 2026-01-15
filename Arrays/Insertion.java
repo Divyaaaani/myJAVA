b688dad488406b669b0a2a03904f9588ae5aa97b
Divyaaaani <divyanikhandait1234@gmail.com>
2026-01-15 16:49:56 +0530

    insertion

--- Arrays/Insertion.java
+++ Arrays/Insertion.java
@@ -1,4 +1,30 @@
 import java.util.*;
-class Sum{
-    
+
+class Insertion {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A[] = new int[10];  // capacity bigger than size
+        A[0]=10; A[1]=20; A[2]=30; A[3]=40; A[4]=50;
+        int size = 5;
+
+       
+
+        System.out.println("Enter element to insert:");
+        int x = sc.nextInt();
+         int index = 2;
+
+        // shift elements to the right
+        for (int i = size; i > index; i--) {
+            A[i] = A[i - 1];
+        }
+
+        // insert element
+        A[index] = x;
+        size++;
+
+        // print array
+        for (int i = 0; i < size; i++) {
+            System.out.print(A[i] + " ");
+        }
+    }
 }
