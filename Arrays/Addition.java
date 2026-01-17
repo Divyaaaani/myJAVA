e0897b8cbbb5977d640a858ac838ffaa0f8443ad
Divyaaaani <divyanikhandait1234@gmail.com>
2026-01-17 15:52:25 +0530

    imp 2nd max

--- Arrays/Addition.java
+++ Arrays/Addition.java
@@ -22,6 +22,30 @@
 //         }
 //     }
 
+// import java.util.*;
+// class Addition
+// {
+//     public static void main(String args[])
+//     {
+//         // methods of creating 2D arrays
+//         int A[][]={{1,2,3},{2,3,4},{4,5,6}}; 
+//         int B[][]={{7,8,9},{1,3,5},{2,4,6}};
+//         int C[][]=new int[3][3];
+
+//         for (int i=0;i<A.length;i++){
+//             for (int j=0; j<A[0].length;j++){
+//                 C[i][j]=A[i][j]-B[i][j]; 
+//             }
+//         }
+//         for (int row[]:C){
+//             for (int x:row){
+//                 System.out.print(x+" ");
+//             }
+//             System.out.println(); //after each row a line gap
+//         }
+//         }
+//     }
+
 import java.util.*;
 class Addition
 {
@@ -34,7 +58,9 @@ class Addition
 
         for (int i=0;i<A.length;i++){
             for (int j=0; j<A[0].length;j++){
-                C[i][j]=A[i][j]-B[i][j]; //* same for multiplication and division
+                for (int k=0; k<A[0].length; k++){
+                    C[i][j]=C[i][j]+A[i][k]*B[k][j];
+                }
             }
         }
         for (int row[]:C){
--- Arrays/Secondmax.java
+++ Arrays/Secondmax.java
@@ -1,49 +1,71 @@
+// // class Secondmax
+// // {
+// //     public static void main(String args[])
+// //     {
+// //         int A[]={99,100,52,9,8,999};
+// //         int max1,max2;
+// //         max1=max2=A[0];
+
+// //         for(int i=0;i<A.length;i++)
+// //         {
+// //             if(A[i]>max1)
+// //             {
+// //                max2=max1;  //store max1 beforehand in max2 coz badme max1 value willbe changed 
+// //                max1=A[i];
+// //             }
+// //             else if(A[i]>max2)
+// //             {
+// //                 max2=A[i];
+// //             }
+// //         }
+// //         System.out.println("second max is: "+max2);
+// //     }
+// // }
+
+// import java.lang.*;
 // class Secondmax
 // {
 //     public static void main(String args[])
 //     {
-//         int A[]={99,100,52,9,8,999};
 //         int max1,max2;
-//         max1=max2=A[0];
-
-//         for(int i=0;i<A.length;i++)
-//         {
-//             if(A[i]>max1)
-//             {
-//                max2=max1;  //store max1 beforehand in max2 coz badme max1 value willbe changed 
-//                max1=A[i];
+//         int A[]={99,100,52,9,8,999};
+//              max1=max2=A[0];
+//         for (int i=0;i<A.length;i++){
+           
+//             if(A[i]>max1){
+//                 max2=max1;
+//                 max1=A[i];
 //             }
-//             else if(A[i]>max2)
-//             {
+//             else if (A[i]>max2){
 //                 max2=A[i];
 //             }
 //         }
-//         System.out.println("second max is: "+max2);
+
+//         System.out.println("second max is: "+max1+","+max2);
+
+
+
 //     }
 // }
-
-import java.lang.*;
-class Secondmax
-{
-    public static void main(String args[])
-    {
-        int max1,max2;
-        int A[]={99,100,52,9,8,999};
-             max1=max2=A[0];
-        for (int i=0;i<A.length;i++){
-           
-            if(A[i]>max1){
-                max2=max1;
-                max1=A[i];
-            }
-            else if (A[i]>max2){
-                max2=A[i];
-            }
+class Secondmax {
+    public static int Secondmax(int[] A) {
+    int max1;
+    int max2;
+    max1=max2=Integer.MIN_VALUE;
+    for (int i=0;i<A.length;i++){
+        if (A[i]>max1){
+            max2=max1;
+            max1=A[i];
         }
-
-        System.out.println("second max is: "+max1+","+max2);
-
-
-
+        else if (A[i]>max2 && A[i]!=max1){
+            max2=A[i];
+        }
+    }return max2;
+    }
+    public static void main(String args[]){
+        // Solution obj=new Solution();
+        int A[]={2,3,6,6,5};
+        System.out.println(Secondmax(A));
+        
     }
 }
